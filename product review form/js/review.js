5a508433563cbf7c138b82458e92fe15e9140210
StanleyMayweather <seberendu@byupathway.edu>
2025-08-14 19:50:55 +0100

    product review form

---
+++ product review form/js/review.js
@@ -0,0 +1,6 @@
+let count = Number(localStorage.getItem('reviewCount')) || 0;
+count++;
+localStorage.setItem('reviewCount', count);
+document.getElementById('reviewCount').textContent = count;
+
+  
